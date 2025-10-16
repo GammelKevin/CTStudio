5ea71889313d57b8f712efd2dac791459ff55e8a
GammelKevin <gammelkevin@github.com>
2025-10-17 00:52:43 +0200

    Fix TypeScript canvas null error in register page
    Apply same canvas dimension variable fix to register page.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -110,6 +110,9 @@ const FloatingParticles: React.FC = () => {
     setCanvasSize();
     window.addEventListener('resize', setCanvasSize);
 
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+
     class Particle {
       x: number;
       y: number;
@@ -119,8 +122,8 @@ const FloatingParticles: React.FC = () => {
       opacity: number;
 
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * canvasWidth;
+        this.y = Math.random() * canvasHeight;
         this.size = Math.random() * 2 + 1;
         this.speedX = (Math.random() - 0.5) * 0.5;
         this.speedY = (Math.random() - 0.5) * 0.5;
@@ -131,10 +134,10 @@ const FloatingParticles: React.FC = () => {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        if (this.x > canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
+        if (this.x > canvasWidth) this.x = 0;
+        if (this.x < 0) this.x = canvasWidth;
+        if (this.y > canvasHeight) this.y = 0;
+        if (this.y < 0) this.y = canvasHeight;
       }
 
       draw() {
@@ -154,7 +157,7 @@ const FloatingParticles: React.FC = () => {
     }
 
     const animate = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
       particles.forEach(particle => {
         particle.update();
